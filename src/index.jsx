961e3f4e87fa6b0b16af135c39723bb7a7b63978
Artem31246 <b11342093@gmail.com>
2026-01-21 23:07:34 +0200

    Added Adaptive CSS

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,28 @@
 import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./styles/app.css";
+import MainPage from "./MainPage";
+import * as Sentry from "@sentry/react";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  sendDefaultPii: true,
+  debug: true
+});
+
+
+const Root = () => {
+  const [gpxFile, setGpxFile] = useState(null);
+
+  
+  if (gpxFile) {
+    return <App initialFile={gpxFile} />;
+  }
+
+
+  
+  return <MainPage onFileSelected={setGpxFile} />;
+};
+
+createRoot(document.getElementById("root")).render(<Root />);
